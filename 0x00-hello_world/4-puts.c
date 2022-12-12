3095b8a8af407faab548a198fe4c177799bd3b62
n6ju <najira.berihun@gmail.com>
2022-12-12 04:49:17 -0800

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 /**
  * main - Entry point
  * Return: Always 0 (Success)
@@ -6,6 +7,6 @@
 int main(void)
 {
 	puts("\"programming is like building a multilingual puzzle");
-	return (0)
+	return (0);
 
 }
