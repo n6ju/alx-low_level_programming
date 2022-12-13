c4588bb9f96ee4081de6d1b389d71363b2609f14
n6ju <najira.berihun@gmail.com>
2022-12-13 02:35:12 -0800

    task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,13 @@
 #include "main.h"
 
 /**
- * main - prints _putchar and a new line
+ * main - Entry point
+ *
  * Return: Always 0
  */
 int main(void)
-{	
+{
+
 	_putchar('_');
 	_putchar('p');
 	_putchar('u');
@@ -17,4 +19,5 @@ int main(void)
 	_putchar('\n');
 
 	return (0);
+
 }
