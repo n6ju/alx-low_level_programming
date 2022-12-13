822188c266a381ded26867baced840f3749196fa
n6ju <najira.berihun@gmail.com>
2022-12-13 03:46:25 -0800

    task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,14 +1,21 @@
 #include "main.h"
 
 /**
- * print_alphabet - print the alpha in lowercase
+ * print_alphabet - Entry point
+ *
+ * Return: 0
  */
+
 void print_alphabet(void)
+
 {
-	char la;
+	char c = 'a';
 
-	for (la = 'a'; la <= 'z'; la++)
-		_putchar(la);
+	while (c <= 'z')
+	{
+		_putchar(c);
+		c++;
+	}
 
-	_putchar('\n')
+	putchar('\n');
 }
