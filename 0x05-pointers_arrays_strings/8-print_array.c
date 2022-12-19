80f215857caa1a9ec1870aaff6ce32cf8578319f
n6ju <najira.berihun@gmail.com>
2022-12-19 07:08:18 -0800

    8 task

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -4,16 +4,16 @@
 /**
  * print_array - prints n elements of an array
  * @a: array
- * @n: numbers of value to be printed
+ * @n: number of values to be printed
  */
 void print_array(int *a, int n)
 {
-	int q;
+	int t;
 
-	for (q = 0; q < n; q++)
+	for (t = 0; t < n; t++)
 	{
-		printf("%d", a[q]);
-		if (q != n - 1)
+		printf("%d", a[t]);
+		if (t != n - 1)
 			printf(", ");
 	}
 
