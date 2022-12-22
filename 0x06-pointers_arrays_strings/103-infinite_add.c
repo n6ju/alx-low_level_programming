5bf9c3f46f05367c97f831ae4fdf05c70e50af30
n6ju <najira.berihun@gmail.com>
2022-12-22 11:48:10 -0800

    11 task

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -3,12 +3,12 @@
 /**
  * infinite_add - C function that adds two numbers stored
  * in strings to a buffer.
- * assumes that strings are never empty and
+ * Assumes that strings are never empty and
  * that numbers always be postive, or 0.
- * assumes there are only digits stored in the number strings.
- * if result can be stored in the buffer,
+ * Assumes there are only digits stored in the number strings.
+ * If result can be stored in the buffer,
  * returns a pointer to the result.
- * if result cannot be stored in the buffer, returns '0'.
+ * If result cannot be stored in the buffer, returns '0'.
  * @n1: first number to be added
  * @n2: second number to be added
  * @r: store result
@@ -21,11 +21,11 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 }
 
 /**
- * add_strings - add the numbers stored in two rings.
- * @n1: the string containing the first number to be added.
- * @n2: the string containing the second number to be added.
- * @r: the buffer to store the result.
- * @r_index: the current index of the buffer.
+ * add_strings - Adds the numbers stored in two rings.
+ * @n1: The string containing the first number to be added.
+ * @n2: The string containing the second number to be added.
+ * @r: The buffer to store the result.
+ * @r_index: The current index of the buffer.
  *
  * Return: if r can store the sum - a pointer to the result.
  *         if r cannot store the sum - 0.
@@ -38,12 +38,12 @@ char *add_strings(char *n1, char *n2, char *r, int r_index)
 	for (; *n1 && *n2; n1--, n2--, r_index--)
 	{
 		num = (*n1 - '0') + (*n2 - '0');
-		num + = tens;
+		num += tens;
 		*(r + r_index) = (num % 10) + '0';
 		tens = num / 10;
 	}
 
-	for (; *n2; n2--; r_index--)
+	for (; *n2; n2--; r_index++)
 	{
 		num = (*n2 - '0') + tens;
 		*(r + r_index) = (num % 10) + '0';
