69de568b0b706e452a844d106283a869fafe5fe6
n6ju <najira.berihun@gmail.com>
2022-12-23 04:46:12 -0800

    12 task

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -1,53 +1,49 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * print_buffer - c function that prints the content of an
- * inputted numbers of bytes from buffer
- * Prints 10 bytesi per line
- * Starts with the position of the fisrt byte in hexadecimal (8 chars),
- * Starting with '0'
- * Eachline shows the hexadecimal content (2 chars) of the buffer,
- * 2 bytes at a time, separated by a space
- * Each line shows the content of the buffer
- * Prints the bytes if it is printable; if not, prints '.'
- * Each line end with a new line '\n'
- * If the inputed byte is 0 or less, the function only prints a new line
- * @b: numbers of bytes
- * @size: size of the bytes
+ * print_buffer - prints buffer
+ * @b: buffer
+ * @size: size
+ * Return: void
  */
+
 void print_buffer(char *b, int size)
 {
-	int i = 0, j;
+	int o, j, i;
 
-	if (size < 0)
+	o = 0;
+
+	if (size <= 0)
 	{
-		printf('\n');
+		printf("\n");
 		return;
 	}
-
-	while (i < size)
+	while (0 < size)
 	{
-		if (i % 10 == 0)
-			printf("%08x: ", i);
-		for (j = i; j < i + 9; j += 2)
+		j = size - o < 10 ? size - o : 10;
+		printf("%08x: ", o);
+		for (i = 0; i < 10; i++)
 		{
-			if ((j < size) && ((j + 1) < size))
-				printf("%02x%02x: ", b[j], b[j + 1]);
+			if (i < j)
+				printf("%02x", *(b + o + i));
 			else
+				printf(" ");
+			if (i % 2)
 			{
-				while (++j <= i + 10)
-					printf(" ");
 				printf(" ");
 			}
 		}
-		for (j = i; j < i + 9 && j < size; j++)
+		for (i = 0; i < j; i++)
 		{
-			if (b[j] >= 32 && b[j] <= 126)
-				printf("%c", b[j]);
-			else
-				printf(".");
+			int c = *(b + o + i);
+
+			if (c < 32 || c > 132)
+			{
+				c = '.';
+			}
+			printf("%c", c);
 		}
-		printf('\n');
-		i += 10;
+		printf("\n");
+		o += 10;
 	}
 }
