44a6313234ae6458e7055bfc2eb4ce82981936b1
n6ju <najira.berihun@gmail.com>
2022-12-23 12:22:10 -0800

    10 task

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,10 +1,5 @@
 #include <stdio.h>
 
-/**
- * type your main function name
- * description: type your main function
- * Return: 0
- */
 int main(void)
 {
   int n;
@@ -25,4 +20,4 @@ int main(void)
   /* ...so that this prints 98\n */
   printf("a[2] = %d\n", a[2]);
   return (0);
-} 
+}
